32c0c9167d348ad01b69640f98d4aa9ba793b325
HungNguyenPQ <quyhung6129@gmail.com>
2023-08-28 10:48:13 +0700

    Adding app constant string and spacing

---
+++ lib/configuration/constants/app_spacing.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+
+/// Padding Constants
+const emptyPadding = EdgeInsets.zero;
+
+const horizontalPadding4 = EdgeInsets.symmetric(horizontal: 4.0);
+const horizontalPadding8 = EdgeInsets.symmetric(horizontal: 8.0);
+const horizontalPadding12 = EdgeInsets.symmetric(horizontal: 12.0);
+const horizontalPadding16 = EdgeInsets.symmetric(horizontal: 16.0);
+const horizontalPadding24 = EdgeInsets.symmetric(horizontal: 24.0);
+const horizontalPadding32 = EdgeInsets.symmetric(horizontal: 32.0);
+const horizontalPadding48 = EdgeInsets.symmetric(horizontal: 48.0);
+
+const verticalPadding2 = EdgeInsets.symmetric(vertical: 2.0);
+const verticalPadding4 = EdgeInsets.symmetric(vertical: 4.0);
+const verticalPadding8 = EdgeInsets.symmetric(vertical: 8.0);
+const verticalPadding12 = EdgeInsets.symmetric(vertical: 12.0);
+const verticalPadding16 = EdgeInsets.symmetric(vertical: 16.0);
+const verticalPadding24 = EdgeInsets.symmetric(vertical: 24.0);
+const verticalPadding32 = EdgeInsets.symmetric(vertical: 32.0);
+const verticalPadding48 = EdgeInsets.symmetric(vertical: 48.0);
+
+const allPadding4 = EdgeInsets.all(4.0);
+const allPadding8 = EdgeInsets.all(8.0);
+const allPadding12 = EdgeInsets.all(12.0);
+const allPadding16 = EdgeInsets.all(16.0);
+const allPadding24 = EdgeInsets.all(24.0);
+const allPadding32 = EdgeInsets.all(32.0);
+const allPadding48 = EdgeInsets.all(48.0);
+
+const topPadding1 = EdgeInsets.only(top: 1.0);
+const topPadding2 = EdgeInsets.only(top: 2.0);
+const topPadding4 = EdgeInsets.only(top: 4.0);
+const topPadding8 = EdgeInsets.only(top: 8.0);
+const topPadding12 = EdgeInsets.only(top: 12.0);
+const topPadding16 = EdgeInsets.only(top: 16.0);
+const topPadding24 = EdgeInsets.only(top: 24.0);
+const topPadding32 = EdgeInsets.only(top: 32.0);
+const topPadding48 = EdgeInsets.only(top: 48.0);
+
+const bottomPadding1 = EdgeInsets.only(bottom: 1.0);
+const bottomPadding2 = EdgeInsets.only(bottom: 2.0);
+const bottomPadding4 = EdgeInsets.only(bottom: 4.0);
+const bottomPadding8 = EdgeInsets.only(bottom: 8.0);
+const bottomPadding12 = EdgeInsets.only(bottom: 12.0);
+const bottomPadding16 = EdgeInsets.only(bottom: 16.0);
+const bottomPadding24 = EdgeInsets.only(bottom: 24.0);
+const bottomPadding32 = EdgeInsets.only(bottom: 32.0);
+const bottomPadding48 = EdgeInsets.only(bottom: 48.0);
+
+const leftPadding4 = EdgeInsets.only(left: 4.0);
+const leftPadding8 = EdgeInsets.only(left: 8.0);
+const leftPadding12 = EdgeInsets.only(left: 12.0);
+const leftPadding16 = EdgeInsets.only(left: 16.0);
+const leftPadding24 = EdgeInsets.only(left: 24.0);
+const leftPadding32 = EdgeInsets.only(left: 32.0);
+const leftPadding48 = EdgeInsets.only(left: 48.0);
+
+const rightPadding4 = EdgeInsets.only(right: 4.0);
+const rightPadding8 = EdgeInsets.only(right: 8.0);
+const rightPadding12 = EdgeInsets.only(right: 12.0);
+const rightPadding16 = EdgeInsets.only(right: 16.0);
+const rightPadding24 = EdgeInsets.only(right: 24.0);
+const rightPadding32 = EdgeInsets.only(right: 32.0);
+const rightPadding48 = EdgeInsets.only(right: 48.0);
+
+// Margins
+
+const horizontalMargin4 = SizedBox(width: 4.0);
+const horizontalMargin8 = SizedBox(width: 8.0);
+const horizontalMargin12 = SizedBox(width: 12.0);
+const horizontalMargin16 = SizedBox(width: 16.0);
+const horizontalMargin24 = SizedBox(width: 24.0);
+const horizontalMargin32 = SizedBox(width: 32.0);
+const horizontalMargin48 = SizedBox(width: 48.0);
+
+const verticalMargin4 = SizedBox(height: 4.0);
+const verticalMargin8 = SizedBox(height: 8.0);
+const verticalMargin12 = SizedBox(height: 12.0);
+const verticalMargin16 = SizedBox(height: 16.0);
+const verticalMargin24 = SizedBox(height: 24.0);
+const verticalMargin32 = SizedBox(height: 32.0);
+const verticalMargin48 = SizedBox(height: 48.0);
+
+// Radius
+const radius4 = Radius.circular(4.0);
+const radius8 = Radius.circular(8.0);
+const radius12 = Radius.circular(12.0);
+const radius16 = Radius.circular(16.0);
+const radius24 = Radius.circular(24.0);
+const radius32 = Radius.circular(32.0);
+const radius48 = Radius.circular(48.0);
+const radius64 = Radius.circular(64.0);
+
+// Border Radius
+const borderRadius0 = BorderRadius.all(Radius.zero);
+
+const borderRadius4 = BorderRadius.all(radius4);
+const borderRadius8 = BorderRadius.all(radius8);
+const borderRadius12 = BorderRadius.all(radius12);
+const borderRadius16 = BorderRadius.all(radius16);
+const borderRadius24 = BorderRadius.all(radius24);
+const borderRadius32 = BorderRadius.all(radius32);
+const borderRadius48 = BorderRadius.all(radius48);
+const borderRadius64 = BorderRadius.all(radius64);
+
+const borderRadiusTopLeft4 = BorderRadius.only(topLeft: radius4);
+const borderRadiusTopLeft8 = BorderRadius.only(topLeft: radius8);
+const borderRadiusTopLeft12 = BorderRadius.only(topLeft: radius12);
+const borderRadiusTopLeft16 = BorderRadius.only(topLeft: radius16);
+const borderRadiusTopLeft24 = BorderRadius.only(topLeft: radius24);
+const borderRadiusTopLeft32 = BorderRadius.only(topLeft: radius32);
+
+const borderRadiusTopRight4 = BorderRadius.only(topRight: radius4);
+const borderRadiusTopRight8 = BorderRadius.only(topRight: radius8);
+const borderRadiusTopRight12 = BorderRadius.only(topRight: radius12);
+const borderRadiusTopRight16 = BorderRadius.only(topRight: radius16);
+const borderRadiusTopRight24 = BorderRadius.only(topRight: radius24);
+const borderRadiusTopRight32 = BorderRadius.only(topRight: radius32);
+
+const borderRadiusBottomLeft4 = BorderRadius.only(bottomLeft: radius4);
+const borderRadiusBottomLeft8 = BorderRadius.only(bottomLeft: radius8);
+const borderRadiusBottomLeft12 = BorderRadius.only(bottomLeft: radius12);
+const borderRadiusBottomLeft16 = BorderRadius.only(bottomLeft: radius16);
+const borderRadiusBottomLeft24 = BorderRadius.only(bottomLeft: radius24);
+const borderRadiusBottomLeft32 = BorderRadius.only(bottomLeft: radius32);
+
+const borderRadiusBottomRight4 = BorderRadius.only(bottomRight: radius4);
+const borderRadiusBottomRight8 = BorderRadius.only(bottomRight: radius8);
+const borderRadiusBottomRight12 = BorderRadius.only(bottomRight: radius12);
+const borderRadiusBottomRight16 = BorderRadius.only(bottomRight: radius16);
+const borderRadiusBottomRight24 = BorderRadius.only(bottomRight: radius24);
+const borderRadiusBottomRight32 = BorderRadius.only(bottomRight: radius32);
+
+// Border
+Border borderAllBlackThin = Border.all(color: Colors.black, width: 0.2);
+Border borderAllBlackMedium = Border.all(color: Colors.black, width: 0.5);
+Border borderAllBlackThick = Border.all(color: Colors.black, width: 1.0);
---
+++ lib/configuration/constants/app_strings.dart
@@ -0,0 +1,33 @@
+const String appTitle = 'iHun Jobfinde';
+
+// Splash Page
+const String splashTitle1 = 'Welcome to';
+const String splashTitle2a = 'We help you find your dream job';
+const String splashTitle2b = 'With more than 1000 jobs available';
+const String splashTitle3a = 'Let\'s get started now!';
+const String splashTitle3b = 'Get started';
+const String skipBtn = 'Skip';
+
+
+// Sign In Page
+const String signInTitle = 'Sign In';
+const String signInSubTitle = 'Please sign in to continue';
+const String signInEmailHint = 'Type in your email';
+const String signInPasswordHint = 'Type in your password';
+const String signInDontHaveAccount = 'Don\'t have an account?';
+
+// Sign Up Page
+const String signUpTitle = 'Sign Up';
+const String signUpSubTitle = 'Please sign up to continue';
+const String signUpEmailHint = 'Type in your email';
+const String signUpPasswordHint = 'Type in your password';
+const String signUpNamehint = 'Type in your name';
+const String signUpAlreadyHaveAccount = 'Already have an account?';
+
+
+// Home Page
+const String homeTitle = 'Home';
+const String homeSearchHint = 'Search for jobs';
+const String homeSuggestions = 'Suggested Jobs for You';
+const String homeRecentJobs = 'Recent Jobs';
+const String homeViewAll = 'View All';
