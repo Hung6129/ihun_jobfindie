15f5a7471cb00dfbc1c2b74c658eb44f9f291cb7
HungNguyenPQ <quyhung6129@gmail.com>
2023-09-02 21:59:37 +0700

    Changes folder

--- lib/features/authenticate/domain/repositories/auth_repository.dart
+++
@@ -1,7 +0,0 @@
-
-import 'package:ihun_jobfindie/shared/models/user_model.dart';
-import 'package:ihun_jobfindie/shared/typedefs/type_defs.dart';
-
-abstract class AuthenticationRepository {
-  FutureEither<UserModel> login(String email, String password);
-}
--- lib/features/jobs/presentation/detail/widgets/widgets.dart
+++
@@ -1,3 +0,0 @@
-export 'job_detail_aboutcompany.dart';
-export 'job_detail_description.dart';
-export 'job_detail_review.dart';
